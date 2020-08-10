ca6792f0f3821eb37d3010aa62eb7615aa3ef47e
Melkiy3231 <mellkiy123@gmail.com>
2020-08-10 22:05:03 +0300

    Initial commit

---
+++ src/main/java/com/yaroslavshchur/Main.java
@@ -0,0 +1,10 @@
+package main.java.com.yaroslavshchur;
+
+import main.java.com.yaroslavshchur.garbage.Executor;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Executor().run();
+    }
+}
